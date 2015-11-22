840c5dbe7435abada1cfa6b5f772363ac8a40412
aiwirad <andi11449@yahoo.com>
2015-11-22 17:57:38 -0500

    final version of programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function is create matrix for Program Assignment 2
+##
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix(), x=numeric()) {
+  rslt <- NULL
+  crtmtrx <- function(b) {
+    m <<- b
+    rslt <<- NULL	
+  }
+  getmtrx <- function() matrix(m,nrow=x,ncol=x)
+  crtinv <- function(solve) rslt <<- solve
+  getinv <- function() rslt
+  list(crtmtrx=crtmtrx, getmtrx= getmtrx, crtinv=crtinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function will check if the inverse matrix is available in cache or not.
+## If not, it will add the inverse matrix based on x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  rslt <- x$getinv()
+  if(!is.null(rslt)){
+    message("getting carslted data")
+    return(rslt)
+  }
+  tm <- x$getmtrx()
+  rslt <- solve(tm, ...)
+  x$crtinv(rslt)
+  rslt
 }
